1a7c07a84b86a353863c88401b18e76b71e36892
Wei Xu <xuwei5@hisilicon.com>
2015-01-13 10:30:23 +0800

    ARM: hip04: enable reboot on hip04 based on Haojian's patch
    Signed-off-by: Haojian Zhuang <haojian.zhuang@linaro.org>
    Signed-off-by: Wei Xu <xuwei5@hisilicon.com>

--- arch/arm/mach-hisi/hisilicon.c
+++ arch/arm/mach-hisi/hisilicon.c
@@ -14,12 +14,17 @@
 #include <linux/clk-provider.h>
 #include <linux/clocksource.h>
 #include <linux/irqchip.h>
+#include <linux/delay.h>
+#include <linux/of_address.h>
+#include <linux/of_platform.h>
 
 #include <asm/mach/arch.h>
 #include <asm/mach/map.h>
 
 #define HI3620_SYSCTRL_PHYS_BASE		0xfc802000
 #define HI3620_SYSCTRL_VIRT_BASE		0xfe802000
+
+static void __iomem *gb3 = NULL; 
 
 /*
  * This table is only for optimization. Since ioremap() could always share
@@ -50,6 +55,55 @@ static const char *hi3xxx_compat[] __initconst = {
 	NULL,
 };
 
+static void __init hip04_init_machine(void)
+{
+	unsigned int data, mask;
+	of_platform_populate(NULL, of_default_bus_match_table, NULL, NULL);
+
+	gb3 = ioremap(0xe4003000, 0x1000);
+	if (!gb3) {
+		pr_err("failed to map GB3\n");
+		return;
+	}
+	mask = 0xffdfffff;
+	/* read GPIO3_SWPORT_DDR */
+	data = readl_relaxed(gb3 + 4);
+	if (!(data & ~mask)) {
+		/* switch GPIO direction from IN to OUT */
+		writel_relaxed(data | ~mask, gb3 + 4);
+	}
+	data = readl_relaxed(gb3);
+	/* write high to GPIO117 to disable watchdog */
+	writel_relaxed(data | ~mask, gb3);
+}
+
+static void hip04_restart(enum reboot_mode mode, const char *cmd)
+{
+	unsigned int data, mask;
+
+	if (!gb3) {
+		pr_err("GB3 isn't initialized\n");
+		return;
+	}
+	mask = 0xffdfffff;
+	/* read GPIO3_SWPORT_DDR */
+	data = readl_relaxed(gb3 + 4);
+	if (!(data & ~mask)) {
+		/* switch GPIO direction from IN to OUT */
+		writel_relaxed(data | ~mask, gb3 + 4);
+	}
+	data = readl_relaxed(gb3);
+	/* write low to GPIO117 */
+	writel_relaxed(data & mask, gb3);
+	udelay(100);
+	/* write high to GPIO117 to disable watchdog */
+	writel_relaxed(data | ~mask, gb3);
+	udelay(100);
+	/* write low to GPIO117 */
+	writel_relaxed(data & mask, gb3);
+	udelay(100);
+}
+
 DT_MACHINE_START(HI3620, "Hisilicon Hi3620 (Flattened Device Tree)")
 	.map_io		= hi3620_map_io,
 	.dt_compat	= hi3xxx_compat,
@@ -71,4 +125,6 @@ static const char *hip04_compat[] __initconst = {
 
 DT_MACHINE_START(HIP04, "Hisilicon HiP04 (Flattened Device Tree)")
 	.dt_compat	= hip04_compat,
+	.init_machine	= hip04_init_machine,
+	.restart	= hip04_restart,
 MACHINE_END
